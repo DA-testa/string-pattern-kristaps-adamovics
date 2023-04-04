82dcf7c2469369eec7cc461c7305e35066c23f40
kristaps-adamovics <124582088+kristaps-adamovics@users.noreply.github.com>
2023-04-04 12:08:03 +0300

    try1

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,54 @@
-# python3
+B = 128
+Q = 37
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    txt=input()
+    if "F" in txt:
+        filename=input()
+        if "a" not in filename:
+            with open(str("test/"+filename), mode="r") as fails:
+                pattern = fails.readline()
+                text = fails.readline()
+            return (pattern.rstrip(), text.rstrip())
+        else:
+            print("error")
+    elif "I" in txt:
+        return (input().rstrip(), input().rstrip())
+    else:
+        print("Input error")
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
     # and return an iterable variable
-    return [0]
+    occurrences = []
+    pat_len = len(pattern)
+    text_len = len(text)
+    pat_hash = get_hash(pattern)
+    mtext = get_hash(text[:pat_len])
+    mult = 1
+    for i in range(1, pat_len):
+        mult = (mult * B) % Q
+    
+    for i in range(text_len - pat_len + 1):
+        if i != 0:
+            # calculate next substring hash
+            mtext = ((mtext - ord(text[i - 1]) * mult) * B + ord(text[i + pat_len - 1])) % Q
+        if pat_hash == mtext:
+            if pattern == text[i:(i + pat_len)]:
+                occurrences.append(i)       
+    # and return an iterable variable
+    return occurrences
+
+def get_hash(pattern: str) -> int:
+    m = len(pattern)
+    result=0
+    for i in range(m):
+        result = (B * result + ord(pattern[i])) % Q
+    return result
 
 
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
